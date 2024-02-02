428acf1f70d98c85e21876fa80b3ea12634f31bc
akayela <akayela22@gmail.com>
2024-02-02 06:54:06 +0200

    Using variadic functions

--- variadic_functions/main.go
+++ variadic_functions/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func severalInts(numbers ...int) {
 	fmt.Println(numbers)
@@ -14,6 +17,16 @@ func mix(num int, flag bool, strings ...string) {
 	fmt.Println(num, flag, strings)
 }
 
+func maximum(numbers ...float64) float64 {
+	max := math.Inf(-1)
+	for _, number := range numbers {
+		if number > max {
+			max = number
+		}
+	}
+	return max
+}
+
 func main() {
 	severalInts(1)
 	severalInts(1, 2, 4, 6)
@@ -21,4 +34,6 @@ func main() {
 	severalStrings("a", "b", "c", "d", "e")
 	mix(1, true, "a", "b")
 	mix(2, false, "d", "e")
+	fmt.Println(maximum(71.8, 56.2, 89.3))
+	fmt.Println(maximum(90.7, 89.7, 98.3, 92.3))
 }
