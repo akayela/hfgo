9b3e6853d27d3f422dc334119505c31789dfdc3d
akayela <akayela22@gmail.com>
2024-02-26 07:07:42 +0200

    slices and maps

---
+++ ch07/07_slice/go.mod
@@ -0,0 +1,3 @@
+module github.com/akayela/count
+
+go 1.21.5
---
+++ ch07/07_slice/main.go
@@ -0,0 +1,33 @@
+// Count tallies the number of times each line occurs within a file
+package main
+
+import (
+	"datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var names []string
+	var counts []int
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+		}
+		if !matched {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+}
