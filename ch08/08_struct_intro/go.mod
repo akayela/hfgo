60494f8236a5c2d39a2e14bcff186ab62d995394
akayela <akayela22@gmail.com>
2024-02-29 21:52:10 +0200

    intro to struct

---
+++ ch08/08_struct_intro/go.mod
@@ -0,0 +1,3 @@
+module github.com/akayela/structs
+
+go 1.21.5
---
+++ ch08/08_struct_intro/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myStruct struct {
+		number float64
+		word   string
+		toggle bool
+	}
+	fmt.Printf("%#v\n", myStruct)
+}
