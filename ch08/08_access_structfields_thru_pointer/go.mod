63525fcb4e25ae2ed8708b2493074a7db1486ac4
akayela <akayela22@gmail.com>
2024-03-05 19:43:34 +0200

    Structs with pointers

---
+++ ch08/08_access_structfields_thru_pointer/go.mod
@@ -0,0 +1,3 @@
+module github.com/akayela/struct-pointer
+
+go 1.21.5
--- ch08/08_def_types_w_func_sub/main.go
+++ ch08/08_def_types_w_func_sub/main.go
@@ -8,25 +8,25 @@ type subscriber struct {
   active bool
 }
 
-func printInfo(s subscriber) {
+func printInfo(s *subscriber) {
   fmt.Println("Name:", s.name)
   fmt.Println("Monthly rate:", s.rate)
   fmt.Println("Active", s.active)
 }
 
-func defaultSubscriber(name string) subscriber {
+func defaultSubscriber(name string) *subscriber {
   var s subscriber
   s.name = name
   s.rate = 5.99
   s.active = true
-  return s
+  return &s
 }
 
 func main() {
-  subscriber1 := defaultSubscriber("Audience Kayela")
-  subscriber1.rate = 4.99
-  printInfo(subscriber1)
+  pointerToSubscriber1 := defaultSubscriber("Audience Kayela")
+  pointerToSubscriber1.rate = 4.99
+  printInfo(pointerToSubscriber1)
 
-  subscriber2 := defaultSubscriber("Joana Kayela")
-  printInfo(subscriber2)
+  pointerToSubscriber2 := defaultSubscriber("Joana Kayela")
+  printInfo(pointerToSubscriber2)
 }
