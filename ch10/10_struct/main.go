c85dc72733e6d4fc87173afd996e8fc809e06a4a
akayela <akayela22@gmail.com>
2024-03-16 21:32:47 +0200

    Setter methods

--- ch10/10_struct/main.go
+++ ch10/10_struct/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+    "log"
+)
 
 type Date struct {
     Year int
@@ -8,22 +12,43 @@ type Date struct {
     Day int
 }
 
-func (d *Date) SetYear(year int) {
+func (d *Date) SetYear(year int) error{
+    if year < 1 {
+        return errors.New("invalid year")
+    }
     d.Year = year
+    return nil
 }
 
-func (d *Date) SetMonth(month int) {
+func (d *Date) SetMonth(month int) error {
+    if month < 1 || month > 12 {
+        return errors.New("invalid month")
+    }
     d.Month = month
+    return nil
 }
 
-func (d *Date) SetDay(day int) {
+func (d *Date) SetDay(day int) error {
+    if day < 1 || day > 31 {
+        return errors.New("invalid day")
+    }
     d.Day = day
+    return nil
 }
 
 func main() {
     date := Date{}
-    date.SetYear(2024)
-    date.SetMonth(3)
-    date.SetDay(16)
+    err := date.SetYear(2024)
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = date.SetMonth(3)
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = date.SetDay(16)
+    if err != nil {
+        log.Fatal(err)
+    }
     fmt.Println(date)
 }
